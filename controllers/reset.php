c1d908ffa686616d05aab9ffca89fdaa08b29c25
Glitch (webd236-blog-lab1-starter) <none>
2020-03-28 13:33:05 +0000

    🏔🌶 Checkpoint ./controllers/reset.php:86128/170 ./database.sql:86128/55 ./views/footer.html:86128/4

--- controllers/reset.php
+++ controllers/reset.php
@@ -2,12 +2,9 @@
   include_once "include/util.php";
 
   function post_index() {
-    ob_start();
     $output = `sqlite3 database.db3 < database.sql 2>&1`;
-    ob_end_clean();
-    $output = ob_get_contents();
     if ($output) {
-      die(nl2br($output));
+      die("<pre style='color:red'>SQLite errors in database.sql\n" . $output . "</pre>");
     }
     redirectRelative("index");
   }
