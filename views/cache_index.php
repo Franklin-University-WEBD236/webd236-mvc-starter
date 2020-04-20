d915698ce1a1ead603e4817a53377b4efdf9bf48
Glitch (webd236-blog-lab1-starter) <none>
2020-04-20 13:16:33 +0000

    👑👕 Checkpoint ./views/index.php:86128/1215

--- views/cache_index.php
+++ views/cache_index.php
@@ -82,8 +82,23 @@
 
 <div class="row">
   <div class="col-lg-12">
-    <h2>This is the main index page</h2>
-    <p>Something should go here</p>
+    <h2>How to use this starter</h2>
+    <p>This application can be used as a starting point for any
+    assignment in WEBD 236 that requires the non-object oriented
+    model-view-controller architecture. It is configured with the
+    framework, an empty database, and some tools. Here are some ways
+    you can use this starter.
+      <ul>
+        <li>Several application level constants are available in <code>include/config.php</code> file. </li>
+        <li>To work with the database, edit the <code><?php echo(htmlentities(CONFIG['databaseFile'] . ".sql")); ?></code> file
+        in the root of the project to create tables, add starter
+        data, etc. Then under the "Tools" menu, click "DB Reset."
+        This will reload the database from the SQL file by running the
+        <code>post_index()</code> function in 
+        <code>controllers/reset.php</code>.</li>
+      </ul>
+    </ul>
+    </p>
   </div>
 </div>
   
